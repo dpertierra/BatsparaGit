9cebec1d22f79983b2debabc9628dcff6c1505ec
Diego Pertierra <dpertierra@hotmail.com>
2017-06-22 13:09:58 -0300

    actualizar

--- todo.bat
+++ todo.bat
@@ -1,4 +1,9 @@
 @echo off
+:inicio
+set /p hacer= Que desea hacer?
+goto %hacer%
+
+:add
 set configN=git config --global user.name "Diego Pertierra"
 set configM=git config --global user.email dpertierra@hotmail.com
 set configcred= git config --global credential.helper wincred
@@ -21,4 +26,23 @@ set "gitmensaje= %gitcommit%%mensaje%"
 %gitmensaje%
 %gitpush%
 echo se actualizo correctamente
+set /p algomas= Desea hacer otra cosa?
+if %algomas% == "Si" | %algomas% == "si" | %algomas% == "SI"(goto start)
+else(EXIT)
+goto start
+
+
+
+:clone
+cls
+set gitclone=git clone
+set /p guardaren= Escriba donde desea clonar el repositorio:
+set cd=cd
+set /p linkrepo= Escriba el link del repositorio que desea clonar:
+set "gitcloneconrepo= %gitclone% %linkrepo%"
+set "guardar= %cd% %guardaren%"
+%guardar%
+%gitcloneconrepo%
+echo se clono correctamente
 pause
+goto start
