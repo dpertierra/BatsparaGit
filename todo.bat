8f1193aa29a3e69e5a579b8f89fe06cc9d6886aa
Diego Pertierra <dpertierra@hotmail.com>
2017-06-22 15:45:37 -0300

    Actulizando

--- todo.bat
+++ todo.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 :inicio
 set /p hacer= Que desea hacer?
 goto %hacer%
@@ -12,8 +13,7 @@ set configcred= git config --global credential.helper wincred
 %configcred%
 cls
 set gitadd=git add
-set apostrofe='
-set gitcommit= git commit -m
+set gitcommit= git commit -m 
 set gitpush= git push
 set cd=cd
 set /p dondeestaelarchivo= Escriba donde esta el archivo o la carpeta que desea agregar:
@@ -21,7 +21,7 @@ set /p archivo= Escriba el nombre del archivo que desea agregar:
 set /p mensaje= Escriba el mensaje para el commit:
 set "gitaddconarchivo= %gitadd% %archivo%"
 set "ubicacion= %cd% %dondeestaelarchivo%"
-set "gitmensaje= %gitcommit% %apostrofe%%mensaje%%apostrofe%"
+set "gitmensaje= %gitcommit% %mensaje%"
 %ubicacion%
 %gitaddconarchivo%
 %gitmensaje%
@@ -72,19 +72,20 @@ goto %algomas%
 cls
 set rm=git rm
 set cd=cd
-set gitcommit= git commit -m
-set apostrofe='
+set gitcommit= git commit -m '
+set apostrofe="'"
 set gitpush= git push
 set /p carpeta= ingrese la ubicacion del archivo:
 set /p archivo=ingrese el nombre del archivo que desea eliminar:
 set /p mensaje= Escriba el mensaje para el commit:
 set "ubicacion=%cd% %carpeta%"
 set "eliminar= %archivo%"
-set "gitmensaje= %gitcommit% %apostrofe%%mensaje%%apostrofe%"
-%ubicacion%
-%eliminar%
-%gitmensaje%
-%gitpush%
+set "gitmensaje= %gitcommit% %mensaje%%apostrofe%"
+echo %gitmensaje%
+REM %ubicacion%
+REM %eliminar%
+REM %gitmensaje%
+REM %gitpush%
 pause
 cls
 set /p algomas= Desea hacer otra cosa?
@@ -104,7 +105,7 @@ set /p archivo=ingrese el nombre de la carpeta que desea eliminar:
 set /p mensaje= Escriba el mensaje para el commit:
 set "ubicacion=%cd% %carpeta%"
 set "eliminar= %archivo%"
-set "gitmensaje= %gitcommit% %apostrofe%%mensaje%%apostrofe%"
+set "gitmensaje= %gitcommit% %mensaje%"
 %ubicacion%
 %eliminar%
 %gitmensaje%
